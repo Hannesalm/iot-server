d6477e592690d77384181028cff5aa3f4834b9c9
Hannes Alm <hannes@almar.se>
2019-03-10 13:49:55 +0100

    added database

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
 
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(":memory:");
+
+db.serialize(function() {
+  db.run("CREATE TABLE reports");
+  //var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+
+  /* for (var i = 0; i < 10; i++) {
+    stmt.run('Ipsum ' + i)
+  } */
+
+  /* stmt.finalize()
+
+  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+    console.log(row.id + ': ' + row.info)
+  }) */
+});
+
+db.close();
+
 app.get("/", (req, res) => {
   res.send("IoT server is working");
 });
