d4a5b84480dcd5311b4dc424fa99679ab5d63b8f
Hannes Alm <hannes@almar.se>
2019-03-10 15:43:50 +0100

    Some changes

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+require("dotenv").config();
 
 var mysql = require("mysql");
 var dbconn = mysql.createConnection({
   host: "localhost",
+  port: "8889",
   user: "root",
   password: "root",
   database: "iot"
 });
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true
+  })
+);
+
+var moment = require("moment");
 
 dbconn.connect(function(err) {
   if (err) {
@@ -17,25 +31,64 @@ dbconn.connect(function(err) {
   }
 });
 
+function checkAlarm() {
+  setTimeout(function() {
+    dbconn.query("SELECT * FROM alarms WHERE status = 'NEW'", function(
+      err,
+      res
+    ) {
+      if (err) throw err;
+      if (res.length > 0) {
+        var id = res[0].id;
+        var CurrentDate = moment().format("YYYY-MM-DD HH:mm:ss");
+        console.log(CurrentDate);
+        var now = moment(res[0].inserted).format("YYYY-MM-DD HH:mm:ss");
+
+        payload = {
+          app_key: process.env.PUSHED_APP_KEY,
+          app_secret: process.env.PUSHED_APP_SECRET,
+          target_type: "app",
+          content: "Brandlarmet har gått i " + res[0].location
+        };
+        //UPDATE table_name SET field1 = new-value1, field2 = new-value2
+        //[WHERE Clause]
+        axios.post("https://api.pushed.co/1/push", payload).then(res => {
+          dbconn.query(
+            "UPDATE alarms SET ? WHERE id ='" + id + "'",
+            {
+              status: "FINISHED",
+              finished_at: now,
+              pushed_message: res.data.response.message
+            },
+            function(err, res) {
+              if (err) throw err;
+            }
+          );
+        });
+      }
+    });
+    checkAlarm();
+  }, 1000);
+}
+
+checkAlarm();
+
 app.get("/", (req, res) => {
   res.send("IoT server is working");
 });
 
-app.get("/test", (req, res) => {
-  var record = {
-    inserted: "2019-03-10 10:00:00",
-    message: "Working"
-  };
+app.post("/alarm", (req, res) => {
+  dbconn.query("INSERT INTO alarms SET ?", req.body, function(err, res) {
+    if (err) throw err;
+  });
+});
 
-  dbconn.query("INSERT INTO reports SET ?", record, function(err, res) {
+app.post("/report", (req, res) => {
+  dbconn.query("INSERT INTO reports SET ?", req.body, function(err, res) {
     if (err) throw err;
 
     console.log("Last record insert id:", res.insertId);
   });
-});
-
-app.post("/post", (req, res) => {
-  console.log("New post generated");
 });
 
 app.listen(3000, () => console.log("IoT server listening on port 3000"));
