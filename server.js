1cf1904f81e01c08cb3a58dfaac2c62d1db97318
Hannes Alm <hannes@almar.se>
2019-03-10 14:13:53 +0100

    added database

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const app = express();
 
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database("database/database.sqlite");
-
-db.serialize(function() {
-  db.run("CREATE TABLE reports");
-  //var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
-
-  /* for (var i = 0; i < 10; i++) {
-    stmt.run('Ipsum ' + i)
-  } */
-
-  /* stmt.finalize()
-
-  db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-    console.log(row.id + ': ' + row.info)
-  }) */
+var mysql = require("mysql");
+var dbconn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "iot"
 });
 
-db.close();
+dbconn.connect(function(err) {
+  if (err) {
+    console.log("Database connection error");
+  } else {
+    console.log("Database connection successful");
+  }
+});
+
+dbconn.end(function(err) {
+  // Function to close database connection
+});
 
 app.get("/", (req, res) => {
   res.send("IoT server is working");
