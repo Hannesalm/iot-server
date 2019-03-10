4bfa1c1b4a79359448f465156d61ac9f3a1f90e3
Hannes Alm <hannes@almar.se>
2019-03-10 14:16:03 +0100

    test

--- server.js
+++ server.js
@@ -25,6 +25,19 @@ app.get("/", (req, res) => {
   res.send("IoT server is working");
 });
 
+app.get("/test", (req, res) => {
+  var record = {
+    inserted: "2019-03-10 10:00:00",
+    message: "Working"
+  };
+
+  dbconn.query("INSERT INTO reports SET ?", record, function(err, res) {
+    if (err) throw err;
+
+    console.log("Last record insert id:", res.insertId);
+  });
+});
+
 app.post("/post", (req, res) => {
   console.log("New post generated");
 });
